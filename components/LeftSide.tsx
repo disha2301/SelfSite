a42587a2e3cacb17d87581784233c2ad5a627a72
Disha <dishay166@gmail.com>
2023-09-06 19:34:45 +0530

    chore:add navbar

---
+++ components/LeftSide.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+// import { TbBrandGithub } from "react-icons/tb";
+import { RxGithubLogo } from "react-icons/rx";
+import { SiInstagram } from "react-icons/si";
+import { SiLinkedin } from "react-icons/si";
+const LeftSide = () => {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-end gap-4 text-textLight">
+      <div className="flex flex-col gap-4">
+        <a href="https://github.com/disha2301" target="_blank">
+          <span
+            className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex
+           items-center justify-center hover:text-textGreen cursor-pointer 
+           hover:-translate-y-2 transition-all duration-300"
+          >
+            <RxGithubLogo />
+          </span>
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/disha-yadav-bb5478221/"
+          target="_blank"
+        >
+          <span
+            className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex
+           items-center justify-center hover:text-textGreen cursor-pointer 
+           hover:-translate-y-2 transition-all duration-300"
+          >
+            <SiLinkedin />
+          </span>
+        </a>
+
+        <a href="https://www.instagram.com/dishaaa._.23/?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D" target="_blank">
+          <span
+            className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex
+           items-center justify-center hover:text-textGreen cursor-pointer 
+           hover:-translate-y-2 transition-all duration-300"
+          >
+            <SiInstagram />
+          </span>
+        </a>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default LeftSide;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import LeftSide from "@/components/LeftSide";
 import Navbar from "@/components/Navbar";
 import { Inter } from "next/font/google";
 
@@ -8,8 +9,15 @@ export default function Home() {
     <>
       <title>Disha's</title>
       <link rel="icon" href="/dishaslogo.jpg"></link>
-      <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight">
+      <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
         <Navbar />
+        <div className="w-full h=[88vh] xl:flex items-center gap-20 justify-between">
+          <div>
+            <LeftSide />
+          </div>
+          <div></div>
+          <div></div>
+        </div>
       </main>
     </>
   );
